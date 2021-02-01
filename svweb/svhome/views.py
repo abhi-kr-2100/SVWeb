4658a1147c87601bc1d2a1858a64c6db5a54d7ba
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-01 21:56:09 +0530

    Add function to display calendar in user's timezone

--- svweb/svhome/views.py
+++ svweb/svhome/views.py
@@ -11,7 +11,26 @@ def index(request):
 def calendar(request):
     """Display the study calendar."""
 
-    return render(request, 'svhome/calendar.html', {})
+    return render(request, 'svhome/calendar.html', {
+        'tz': 'Europe/Brussels',
+        'err': None
+    })
+
+
+def calendar_tz(request, country):
+    """Display the study calendar in the given country's timezone."""
+
+    tz = get_timezone(country)
+    if tz is not None:
+        return render(request, 'svhome/calendar.html', {
+            'tz': tz,
+            'err': None
+        })
+    else:
+        return render(request, 'svhome/calendar.html', {
+            'tz': 'Europe/Brussels',
+            'err': f'{country}'
+        })
 
 
 def study_tips(request):
@@ -30,3 +49,17 @@ def about(request):
     "About Study Vibes."
 
     return render(request, 'svhome/about.html', {})
+
+
+def get_timezone(country):
+    """Return the timezone for the given country."""
+
+    timezones = {
+        'america':  'US/Eastern',
+        'us':       'US/Eastern',
+        'belgium':  'Europe/Brussels',
+        'turkey':   'Europe/Istanbul',
+        'india':    'Asia/Kolkata'
+    }
+
+    return timezones.get(country.lower())
