d3e3d8e2d6b8370611a0b50b2e0226db86490071
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-01 11:38:45 +0530

    Add the calendar view

--- svweb/svhome/views.py
+++ svweb/svhome/views.py
@@ -5,3 +5,8 @@ def index(request):
     """Display the homepage."""
 
     return render(request, 'svhome/index.html', {})
+
+def calendar(request):
+    """Display the study calendar."""
+
+    return render(request, 'svhome/calendar.html', {})
