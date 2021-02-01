ac6ac767b822a4681d3c7059edebcb27ef1dd62e
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-01 11:22:04 +0530

    Fix the svhome.index view

--- svweb/svhome/views.py
+++ svweb/svhome/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
     """Display the homepage."""
 
-    render(request, 'svhome/index.html', {})
+    return render(request, 'svhome/index.html', {})
