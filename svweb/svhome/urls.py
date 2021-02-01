3c9958e65823c5d17482b45db0fcc7bc3132f1aa
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-01 12:45:36 +0530

    Register svhome view urls

--- svweb/svhome/urls.py
+++ svweb/svhome/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
-
+app_name = 'svhome'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('calendar/', views.calendar, name='calendar')
+    path('calendar/', views.calendar, name='calendar'),
+    path('study-tips/', views.study_tips, name='study-tips'),
+    path('social-media/', views.social_media, name='social-media'),
+    path('about/', views.about, name='about')
 ]
