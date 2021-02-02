d3b8dacc89db589b97dc2a10af71f6645772d4ad
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-02 18:00:01 +0530

    Use only one calendar view

--- svweb/svhome/views.py
+++ svweb/svhome/views.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
@@ -8,34 +7,22 @@ def index(request):
 
     return render(request, 'svhome/index.html', {})
 
-
-def calendar(request):
-    """Display the study calendar."""
-
-    return render(request, 'svhome/calendar.html', {
-        'tz': 'Europe/Brussels',
-        'err': None,
-        'timezones': sorted(c.capitalize() for c in settings.TIMEZONES.keys())
-    })
-
-
-def calendar_tz(request, country):
+def calendar(request, country="Belgium"):
     """Display the study calendar in the given country's timezone."""
 
     tz = get_timezone(country)
-    if tz is not None:
-        return render(request, 'svhome/calendar.html', {
-            'tz': tz,
-            'err': None,
-            'timezones': sorted(c.capitalize() for c in settings.TIMEZONES.keys())
-        })
-    else:
-        return render(request, 'svhome/calendar.html', {
-            'tz': 'Europe/Brussels',
-            'err': f'{country}',
-            'timezones': sorted(c.capitalize() for c in settings.TIMEZONES.keys())
-        })
+    countries = get_countries()
+    err = None
 
+    if tz is None:
+        tz = get_timezone('Belgium')
+        err = country
+
+    return render(request, 'svhome/calendar.html', {
+        'tz': tz,
+        'err': err,
+        'timezones': countries
+    })
 
 def study_tips(request):
     """Display a list of saved study tips."""
@@ -58,4 +45,9 @@ def about(request):
 def get_timezone(country):
     """Return the timezone for the given country."""
 
-    return settings.TIMEZONES.get(country.lower())
+    return settings.TIMEZONES.get(country.replace(" ", "-"))
+
+def get_countries():
+    """Return all countries for which TZ data is available."""
+
+    return [c.replace("-", " ") for c in settings.TIMEZONES.keys()]
