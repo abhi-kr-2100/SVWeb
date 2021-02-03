7cd9c86f0692690d8b08dc7ce0a29732a44ff522
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-02-03 17:26:59 +0530

    Add, migrate and register the StudyTip model

--- svweb/svhome/models.py
+++ svweb/svhome/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class StudyTip(models.Model):
+    text = models.CharField(max_length=500)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
